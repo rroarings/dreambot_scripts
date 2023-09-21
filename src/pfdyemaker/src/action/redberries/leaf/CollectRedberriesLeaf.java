db56565214d56c544785bb5b60356db8d8925d78
ovlack <deceivedfx@gmail.com>
2023-09-21 15:18:11 -0500

    refactor: update redberry collecting

--- src/pfdyemaker/src/action/redberries/leaf/CollectRedberriesLeaf.java
+++ src/pfdyemaker/src/action/redberries/leaf/CollectRedberriesLeaf.java
@@ -1,49 +1,55 @@
 package pfdyemaker.src.action.redberries.leaf;
 
-import org.dreambot.api.Client;
+import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.interactive.GameObjects;
 import org.dreambot.api.methods.interactive.Players;
-import org.dreambot.api.methods.world.World;
-import org.dreambot.api.methods.world.Worlds;
-import org.dreambot.api.methods.worldhopper.WorldHopper;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.wrappers.interactive.GameObject;
+import pfdyemaker.src.action.util.QuickMethods;
 import pfdyemaker.src.data.DyeMakerConfig;
 
 public class CollectRedberriesLeaf extends Leaf {
 
     DyeMakerConfig config = DyeMakerConfig.getDyeMakerConfig();
+    private boolean worldHopped = false; // Initialize the flag
 
     private final int REDBERRY_BUSH_ID = 23628;
+    private final int REDBERRY_BUSH_ID2 = 23629;
     private GameObject REDBERRY_BUSH;
-    int distanceThreshold = 8;
 
     @Override
     public boolean isValid() {
-        boolean isNearBush = Players.getLocal().distance(REDBERRY_BUSH) <= distanceThreshold;
-        return config.REDBERRY_AREA.contains(Players.getLocal()) && !Inventory.isFull() && isNearBush;
+        return config.REDBERRY_AREA.contains(Players.getLocal()) && !Inventory.isFull();
     }
 
     @Override
     public int onLoop() {
-        World worldToHop = Worlds.getRandomWorld(world -> world != null && world.isF2P() && world.isNormal() && world.getMinimumLevel() <= Players.getLocal().getLevel());
-        REDBERRY_BUSH = GameObjects.closest(BerryBush -> BerryBush.getID() == REDBERRY_BUSH_ID && BerryBush.distance() <= distanceThreshold);
+        REDBERRY_BUSH = GameObjects.closest(BerryBush -> BerryBush.getID() == REDBERRY_BUSH_ID || BerryBush.getID() == REDBERRY_BUSH_ID2);
+        QuickMethods.drinkEnergyPotion();
 
-        if (REDBERRY_BUSH == null) {
-            config.setStatus("Hopping worlds");
-            WorldHopper.hopWorld(worldToHop);
-            Sleep.sleepUntil(Client::isLoggedIn, 8000, 100);
+        if (!worldHopped && REDBERRY_BUSH == null) {
+            QuickMethods.worldHop();
+            worldHopped = true;
+        } else {
+            worldHopped = false;
         }
 
-        if (REDBERRY_BUSH != null && REDBERRY_BUSH.getID() != 23630) {
+        if (REDBERRY_BUSH != null) {
             if (REDBERRY_BUSH.interact("Pick-from")) {
                 config.setStatus("Picking redberry bush");
-                Sleep.sleepUntil(() -> !Players.getLocal().isAnimating(), 5000, 1000);
+                Sleep.sleep(Calculations.random(900, 1200));
+                Sleep.sleepUntil(this::isPlayerNotAnimating, 8000, 3000);
+                worldHopped = false;
+                return 600;
             }
         }
-        return 800;
+
+        return 600;
     }
 
+    private boolean isPlayerNotAnimating() {
+        return !Players.getLocal().isAnimating();
+    }
 }
