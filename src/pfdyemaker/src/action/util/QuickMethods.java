112d88ab53df3a9b1bf75617b6d964a631436e05
ovlack <deceivedfx@gmail.com>
2023-09-21 15:17:49 -0500

    refactor: add quickmethod utils

---
+++ src/pfdyemaker/src/action/util/QuickMethods.java
@@ -0,0 +1,46 @@
+package pfdyemaker.src.action.util;
+
+import org.dreambot.api.Client;
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.api.methods.world.World;
+import org.dreambot.api.methods.world.Worlds;
+import org.dreambot.api.methods.worldhopper.WorldHopper;
+import org.dreambot.api.utilities.Sleep;
+import pfdyemaker.src.data.DyeMakerConfig;
+
+public class QuickMethods {
+
+    public static void withdrawEnergyPotions() {
+        if (DyeMakerConfig.isUseEnergyPotions()) {
+            DyeMakerConfig.getDyeMakerConfig().setStatus("Withdrawing potions");
+            if (Bank.contains(item -> item.getName().contains("Energy potion("))) {
+                Bank.withdraw(item -> item.getName().contains("Energy potion("), 5);
+                Bank.close();
+            }
+        }
+    }
+
+    public static void drinkEnergyPotion() {
+        int currentEnergy = Walking.getRunEnergy();
+        if (DyeMakerConfig.isUseEnergyPotions()) {
+            if (currentEnergy <= Walking.getRunThreshold() && Inventory.contains(item -> item.getName().contains("Energy potion("))) {
+                Inventory.interact(item -> item.getName().contains("Energy potion("), "Drink");
+                Sleep.sleepUntil(() -> Walking.getRunEnergy() >= 40, 3000, 600);
+            }
+        }
+    }
+
+    public static void worldHop() {
+        World cWorld = Worlds.getCurrent();
+        World worldToHop = Worlds.getRandomWorld(world -> world != null && world.isF2P() && world.isNormal() && world.getMinimumLevel() <= Players.getLocal().getLevel());
+        int delay = Calculations.random(DyeMakerConfig.getWorldHopDelayMin(), DyeMakerConfig.getWorldHopDelayMax());
+        Sleep.sleep(delay);
+        DyeMakerConfig.getDyeMakerConfig().setStatus("Hopping worlds");
+        WorldHopper.hopWorld(worldToHop);
+        Sleep.sleepUntil(() -> Worlds.getCurrent() != cWorld || Client.isLoggedIn(), 8000, 4000);
+    }
+}
