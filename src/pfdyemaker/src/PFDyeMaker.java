b4c2f775bcc276c12921b09ee0f0c6584542e5e6
ovlack <deceivedfx@gmail.com>
2023-09-21 15:18:35 -0500

    refactor: update dyemaker main

--- src/pfdyemaker/src/PFDyeMaker.java
+++ src/pfdyemaker/src/PFDyeMaker.java
@@ -1,127 +1,56 @@
 package pfdyemaker.src;
 
+import org.dreambot.api.Client;
+import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.script.frameworks.treebranch.TreeScript;
-import org.dreambot.api.utilities.Timer;
 import pfdyemaker.src.data.DyeMakerConfig;
-import pfdyemaker.src.ui.DyeMakerUI;
-import pfdyemaker.src.util.QuantityFormatter;
+import pfdyemaker.src.paint.PaintHandler;
+import pfdyemaker.src.ui.Frame;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.IOException;
-import java.net.URL;
 
-@ScriptManifest(category = Category.MONEYMAKING, name = "PF Dye Maker", author = "pharaoh", version = 1.3, image = "https://i.imgur.com/zQ1Vwvt.png")
-public class PFDyeMaker extends TreeScript implements MouseListener {
+@ScriptManifest(category = Category.MONEYMAKING, name = "PF Dye Maker", author = "pharaoh", version = 1.5, image = "https://i.imgur.com/zQ1Vwvt.png")
+public class PFDyeMaker extends TreeScript {
 
-    private boolean hidePaint = false;
-    private final Rectangle PAINT_BUTTON = new Rectangle(475, 458, 40, 18);
     private long startTime;
-    private Timer timer;
-    private Image image;
-    private final RenderingHints AA = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    private final DyeMakerConfig config = DyeMakerConfig.getDyeMakerConfig();
+    private PaintHandler paintHandler;
+
+    public long getStartTime() {
+        return startTime;
+    }
 
     private void initTree() {
-        addBranches(DyeMakerUI.getSelectedItem().getActionBranch());
+        addBranches(Frame.getSelectedItem().getActionBranch());
     }
 
     @Override
     public void onStart() {
-        timer = new Timer();
-        SwingUtilities.invokeLater(() -> new DyeMakerUI().setVisible(true));
+        paintHandler = new PaintHandler(this);
+        Client.getCanvas().addMouseListener(paintHandler);
+        SwingUtilities.invokeLater(() -> new Frame().setVisible(true));
         startTime = System.currentTimeMillis();
-
-        try {
-            image = ImageIO.read(new URL("https://i.imgur.com/vHCUz94.png"));
-        } catch (IOException e) {
-            log("Failed to load image");
-        }
     }
 
     @Override
     public int onLoop() {
-        if (DyeMakerUI.isStartLoop()) {
+        if (Frame.isStartLoop()) {
+            if (DyeMakerConfig.isUseEnergyPotions()) {
+                Walking.setRunThreshold(20);
+            }
             initTree();
         }
-        if (config.getPricedItem() != null) {
-            config.getPricedItem().update();
+        if (DyeMakerConfig.getDyeMakerConfig().getPricedItem() != null) {
+            DyeMakerConfig.getDyeMakerConfig().getPricedItem().update();
         }
         return this.getRoot().onLoop();
     }
 
     @Override
     public void onPaint(Graphics g) {
-        long runTime = System.currentTimeMillis() - startTime;
-        int alpha = 127;
-        int x = 12;
-
-        Graphics2D graphics2D = (Graphics2D) g;
-        Color opaqueBlack = new Color(0, 0, 0, alpha);
-
-        graphics2D.setRenderingHints(AA);
-        graphics2D.setFont(new Font("Bahnschrift", Font.PLAIN, 14));
-
-        if (!hidePaint) {
-            if (DyeMakerUI.isStartLoop()) {
-                if (getCurrentLeafName() != null) {
-                    if (image != null) g.drawImage(image, 0, 0, null);
-
-                    graphics2D.setColor(opaqueBlack);
-                    graphics2D.fillRoundRect(5, 342, 510, 134, 2, 2);
-
-                    graphics2D.setColor(Color.ORANGE);
-                    graphics2D.draw(PAINT_BUTTON);
-                    graphics2D.drawString(getManifest().name(), 380, 473);
-
-                    graphics2D.setColor(Color.WHITE);
-                    graphics2D.drawString("Time: " + Timer.formatTime(runTime), x, 360);
-                    graphics2D.drawString("Status: " + config.getStatus(), x, 380);
-                    graphics2D.drawString("v" + getVersion(), 480+4, 473);
-
-                    if (config.getPricedItem() != null) {
-                        String text = config.getDyeToMake() != null ? "Dyes" : "Items";
-                        int itemsPH = (int) (config.getPricedItem().getAmount() / ((System.currentTimeMillis() - startTime) / 3600000.0D));
-
-                        config.setProfit(config.getPricedItem().getAmount() * config.getPricedItem().getPrice());
-                        graphics2D.drawString(text + ": " + config.getPricedItem().getAmount() + " (" + QuantityFormatter.formatNumber(itemsPH) + ")", x, 400);
-                        graphics2D.drawString("Gold: " + QuantityFormatter.formatNumber(config.getProfit()) + " (" + QuantityFormatter.formatNumber(timer.getHourlyRate(config.getProfit())) + ")", x, 420);
-                    }
-                }
-            }
-        } else {
-            graphics2D.setColor(Color.RED);
-            graphics2D.drawString("Paint hidden", PAINT_BUTTON.x - 45, PAINT_BUTTON.y + 14);
-        }
+        paintHandler.paint(g);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (PAINT_BUTTON.contains(e.getPoint()))
-            hidePaint = !hidePaint;
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
 }
