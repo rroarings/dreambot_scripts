21e057bbc389fab24600b780a0284b1586a14f1e
ovlack <deceivedfx@gmail.com>
2023-09-21 15:18:17 -0500

    refactor: update redberry walking

--- src/pfdyemaker/src/action/redberries/leaf/WalkToRedberriesLeaf.java
+++ src/pfdyemaker/src/action/redberries/leaf/WalkToRedberriesLeaf.java
@@ -5,6 +5,7 @@ import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
 import org.dreambot.api.utilities.Sleep;
+import pfdyemaker.src.action.util.QuickMethods;
 import pfdyemaker.src.data.DyeMakerConfig;
 
 public class WalkToRedberriesLeaf extends Leaf {
@@ -18,10 +19,11 @@ public class WalkToRedberriesLeaf extends Leaf {
 
     @Override
     public int onLoop() {
+
         if (Walking.shouldWalk()) {
             config.setStatus("Walking to redberries");
             Walking.walk(config.REDBERRY_AREA.getRandomTile());
-            Sleep.sleepUntil(() -> config.REDBERRY_AREA.distance(Players.getLocal().getTile()) > 3, 1000);
+            QuickMethods.drinkEnergyPotion();
         }
         return 600;
     }
