5cb84a6967c44cb46c3170a04212b7c013d7b9e5
roarings <onlytainted@gmail.com>
2024-01-09 11:14:31 -0600

    update quickmethods

--- src/pfdyemaker/src/action/util/QuickMethods.java
+++ src/pfdyemaker/src/action/util/QuickMethods.java
@@ -5,6 +5,8 @@ import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.tabs.Tab;
+import org.dreambot.api.methods.tabs.Tabs;
 import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.methods.world.World;
 import org.dreambot.api.methods.world.Worlds;
@@ -18,7 +20,7 @@ public class QuickMethods {
         if (DyeMakerConfig.isUseEnergyPotions()) {
             DyeMakerConfig.getDyeMakerConfig().setStatus("Withdrawing potions");
             if (Bank.contains(item -> item.getName().contains("Energy potion("))) {
-                Bank.withdraw(item -> item.getName().contains("Energy potion("), 5);
+                Bank.withdraw(item -> item.getName().contains("Energy potion("), 3);
                 Bank.close();
             }
         }
@@ -27,9 +29,11 @@ public class QuickMethods {
     public static void drinkEnergyPotion() {
         int currentEnergy = Walking.getRunEnergy();
         if (DyeMakerConfig.isUseEnergyPotions()) {
-            if (currentEnergy <= Walking.getRunThreshold() && Inventory.contains(item -> item.getName().contains("Energy potion("))) {
-                Inventory.interact(item -> item.getName().contains("Energy potion("), "Drink");
-                Sleep.sleepUntil(() -> Walking.getRunEnergy() >= 40, 3000, 600);
+            if (!Bank.isOpen()) {
+                if (currentEnergy <= Walking.getRunThreshold() && Inventory.contains(item -> item.getName().contains("Energy potion("))) {
+                    Inventory.interact(item -> item.getName().contains("Energy potion("), "Drink");
+                    Sleep.sleepUntil(() -> Walking.getRunEnergy() >= 40, 3000, 600);
+                }
             }
         }
     }
@@ -39,8 +43,8 @@ public class QuickMethods {
         World worldToHop = Worlds.getRandomWorld(world -> world != null && world.isF2P() && world.isNormal() && world.getMinimumLevel() <= Players.getLocal().getLevel());
         int delay = Calculations.random(DyeMakerConfig.getWorldHopDelayMin(), DyeMakerConfig.getWorldHopDelayMax());
         Sleep.sleep(delay);
-        DyeMakerConfig.getDyeMakerConfig().setStatus("Hopping worlds");
+        DyeMakerConfig.getDyeMakerConfig().setStatus("Hopping worlds, delay: " + delay);
         WorldHopper.hopWorld(worldToHop);
-        Sleep.sleepUntil(() -> Worlds.getCurrent() != cWorld || Client.isLoggedIn(), 8000, 4000);
+        Sleep.sleepUntil(() -> Worlds.getCurrent() != cWorld || Client.isLoggedIn(), 8000, 300);
     }
 }
