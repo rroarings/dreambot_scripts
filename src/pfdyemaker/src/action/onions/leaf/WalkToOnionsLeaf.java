a4fbacaed170daa5015d6328f690b2a4db8d7991
roarings <onlytainted@gmail.com>
2024-04-02 11:32:58 -0500

    optimize imports

--- src/pfdyemaker/src/action/onions/leaf/WalkToOnionsLeaf.java
+++ src/pfdyemaker/src/action/onions/leaf/WalkToOnionsLeaf.java
@@ -1,7 +1,6 @@
 package pfdyemaker.src.action.onions.leaf;
 
 import org.dreambot.api.methods.container.impl.Inventory;
-import org.dreambot.api.methods.container.impl.bank.BankLocation;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
