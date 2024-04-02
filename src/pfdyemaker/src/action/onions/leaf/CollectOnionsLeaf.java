ba4e0ea9497cede461bb7dcb44e4fd15fa1af035
roarings <onlytainted@gmail.com>
2024-04-02 11:29:49 -0500

    update onion collection

--- src/pfdyemaker/src/action/onions/leaf/CollectOnionsLeaf.java
+++ src/pfdyemaker/src/action/onions/leaf/CollectOnionsLeaf.java
@@ -4,13 +4,16 @@ import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.interactive.GameObjects;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
+import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.utilities.Timer;
 import org.dreambot.api.wrappers.interactive.GameObject;
 import pfdyemaker.src.data.DyeMakerConfig;
 
 public class CollectOnionsLeaf extends Leaf {
 
     DyeMakerConfig config = DyeMakerConfig.getDyeMakerConfig();
+    private int previousOnionCount = 0;
 
     @Override
     public boolean isValid() {
@@ -19,21 +22,27 @@ public class CollectOnionsLeaf extends Leaf {
 
     @Override
     public int onLoop() {
-        GameObject ONION = GameObjects.closest(gameObject -> gameObject.getName().contains("Onion") && gameObject.hasAction("Pick"));
-
-        if (ONION == null) return 600;
-
-        if (ONION.interact("Pick")) {
+        GameObject onion = GameObjects.closest(gameObject -> gameObject.getName().contains("Onion") && gameObject.hasAction("Pick"));
+        if (onion != null && onion.interact("Pick")) {
             config.setStatus("Picking onion");
-            Sleep.sleepUntil(() -> !Players.getLocal().isAnimating(), 900, 1000);
-            config.pricedItem.update();
-        }
-
-        if (Inventory.contains("Onion seed")) {
-            config.setStatus("Dropping onion seed");
-            Inventory.interact("Onion seed", "Drop");
-            Sleep.sleepUntil(() -> !Inventory.contains("Onion seed"), 600);
+            Logger.log("(onions) picking onion...");
+            waitForOnionInInventory();
         }
         return 600;
+    }
+
+    private void waitForOnionInInventory() {
+        Timer timer = new Timer();
+        while (timer.elapsed() < 3000) {
+            int currentOnionCount = Inventory.count("Onion");
+            if (currentOnionCount > previousOnionCount) {
+                config.setStatus("Picking wait");
+                Logger.log("(onions) waited for onion in inventory, current count: " + currentOnionCount);
+                previousOnionCount = currentOnionCount;
+                return;
+            }
+            Sleep.sleep(600); // Adjust the sleep time as needed
+        }
+        Logger.log("(onions) timed out waiting for onion in inventory");
     }
 }
