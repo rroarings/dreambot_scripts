370a1930549ea9d4377c27f3d03336263281b615
roarings <onlytainted@gmail.com>
2024-04-02 11:30:18 -0500

    update onion patch navigation

--- src/pfdyemaker/src/action/onions/leaf/WalkToOnionsLeaf.java
+++ src/pfdyemaker/src/action/onions/leaf/WalkToOnionsLeaf.java
@@ -1,11 +1,14 @@
 package pfdyemaker.src.action.onions.leaf;
 
 import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.BankLocation;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
+import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
 import pfdyemaker.src.data.DyeMakerConfig;
+
 public class WalkToOnionsLeaf extends Leaf {
 
     DyeMakerConfig config = DyeMakerConfig.getDyeMakerConfig();
@@ -15,14 +18,13 @@ public class WalkToOnionsLeaf extends Leaf {
         return !Inventory.isFull() && !config.ONION_AREA.contains(Players.getLocal());
     }
 
-    //todo add combat checks for Draynor prison guards
-
     @Override
     public int onLoop() {
         if (Walking.shouldWalk()) {
             config.setStatus("Walking to onion patch");
+            Logger.log("(navigation) walking to onion patch");
             Walking.walk(config.ONION_AREA.getCenter());
-            Sleep.sleepUntil(() -> config.ONION_AREA.contains(Players.getLocal().getTile()), 1000);
+            Sleep.sleepUntil(() -> config.ONION_AREA.contains(Players.getLocal().getTile()), 6000, 600);
         }
         return 600;
     }
