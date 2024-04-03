42e9e302c31c0bd83e805be21cea477c05498111
roarings <onlytainted@gmail.com>
2024-04-03 14:51:16 -0500

    refactor: update dyemaker main

--- src/pfdyemaker/src/PFDyeMaker.java
+++ src/pfdyemaker/src/PFDyeMaker.java
@@ -1,10 +1,11 @@
 package pfdyemaker.src;
 
 import org.dreambot.api.Client;
-import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManager;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.script.frameworks.treebranch.TreeScript;
+import org.dreambot.api.utilities.Timer;
 import pfdyemaker.src.data.DyeMakerConfig;
 import pfdyemaker.src.paint.PaintHandler;
 import pfdyemaker.src.ui.Frame;
@@ -12,36 +13,58 @@ import pfdyemaker.src.ui.Frame;
 import javax.swing.*;
 import java.awt.*;
 
-@ScriptManifest(category = Category.MONEYMAKING, name = "PF Dye Maker", author = "pharaoh", version = 1.6, image = "https://i.imgur.com/zQ1Vwvt.png")
+@ScriptManifest(category = Category.MONEYMAKING, name = "PF Dye Maker", author = "pharaoh", version = 1.7, image = "https://i.imgur.com/zQ1Vwvt.png")
 public class PFDyeMaker extends TreeScript {
 
+    private Timer timer;
     private long startTime;
     private PaintHandler paintHandler;
+    private Frame gui;
 
-    public long getStartTime() {
-        return startTime;
+    public Timer getTimer() {
+        return timer;
     }
 
     private void initTree() {
-        addBranches(Frame.getSelectedItem().getActionBranch());
+        addBranches(gui.getSelectedBranch().getActionBranch());
     }
 
     @Override
     public void onStart() {
+        timer = new Timer();
         paintHandler = new PaintHandler(this);
         Client.getCanvas().addMouseListener(paintHandler);
-        SwingUtilities.invokeLater(() -> new Frame().setVisible(true));
+        SwingUtilities.invokeLater(() -> {
+            gui = new Frame(); // Initialize the gui field
+            gui.setVisible(true);
+        });
         startTime = System.currentTimeMillis();
+        timer.start();
+    }
+
+    @Override
+    public void onExit() {
+        if (gui != null) {
+            if (gui.isVisible()) {
+                gui.dispose();
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (timer != null) {
+            timer.pause();
+        }
     }
 
     @Override
     public int onLoop() {
-        if (Frame.isStartLoop()) {
-            if (DyeMakerConfig.isUseEnergyPotions()) {
-                Walking.setRunThreshold(20);
-            }
-            initTree();
-        }
+       if (gui != null) {
+           if (gui.isStartLoop()) {
+               initTree();
+           }
+       }
         if (DyeMakerConfig.getDyeMakerConfig().getPricedItem() != null) {
             DyeMakerConfig.getDyeMakerConfig().getPricedItem().update();
         }
