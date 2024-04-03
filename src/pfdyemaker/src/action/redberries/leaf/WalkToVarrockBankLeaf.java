19355100b51e3fc31eb03d2c92f288a32fe865ec
roarings <onlytainted@gmail.com>
2024-04-03 14:38:25 -0500

    refactor: remove potion handling from varrock bank walking

--- src/pfdyemaker/src/action/redberries/leaf/WalkToVarrockBankLeaf.java
+++ src/pfdyemaker/src/action/redberries/leaf/WalkToVarrockBankLeaf.java
@@ -5,8 +5,6 @@ import org.dreambot.api.methods.container.impl.bank.BankLocation;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
-import org.dreambot.api.utilities.Sleep;
-import pfdyemaker.src.action.util.QuickMethods;
 import pfdyemaker.src.data.DyeMakerConfig;
 
 public class WalkToVarrockBankLeaf extends Leaf {
@@ -23,7 +21,6 @@ public class WalkToVarrockBankLeaf extends Leaf {
         if (Walking.shouldWalk()) {
             config.setStatus("Walking to bank");
             Walking.walk(BankLocation.VARROCK_EAST.getArea(1).getCenter());
-            QuickMethods.drinkEnergyPotion();
         }
         return 600;
     }
