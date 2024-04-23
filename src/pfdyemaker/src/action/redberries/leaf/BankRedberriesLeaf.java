b6c2d1cc2816d066b0828b51a9738160c6758444
roarings <onlytainted@gmail.com>
2024-04-23 11:11:21 -0500

    refactor: update redberry banking logic

--- src/pfdyemaker/src/action/redberries/leaf/BankRedberriesLeaf.java
+++ src/pfdyemaker/src/action/redberries/leaf/BankRedberriesLeaf.java
@@ -5,6 +5,7 @@ import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.container.impl.bank.BankLocation;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.script.frameworks.treebranch.Leaf;
+import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
 import pfdyemaker.src.data.DyeMakerConfig;
 
@@ -12,23 +13,27 @@ public class BankRedberriesLeaf extends Leaf {
 
     @Override
     public boolean isValid() {
-        return BankLocation.VARROCK_EAST.getArea(1).contains(Players.getLocal()) && Inventory.contains("Redberries");
+        return BankLocation.VARROCK_EAST.getArea(5).contains(Players.getLocal()) && Inventory.contains("Redberries");
     }
 
     @Override
     public int onLoop() {
         if (!Bank.isOpen()) {
             DyeMakerConfig.dyeConfig().setStatus("Opening bank");
-            Bank.open();
-            Sleep.sleepUntil(Bank::isOpen, 4000, 600);
+            if (Bank.open()) {
+                Sleep.sleepUntil(Bank::isOpen, 5000, 600);
+                Logger.log("(dyes) (bankRedberries) opened bank");
+            }
         }
 
         if (Bank.isOpen()) {
-            DyeMakerConfig.dyeConfig().setStatus("Deposit inventory");
-            Bank.depositAllItems();
-            Sleep.sleepUntil(Inventory::isEmpty, 4000, 600);
+            DyeMakerConfig.dyeConfig().setStatus("Depositing redberries");
+            if (Bank.depositAllItems()) {
+                Sleep.sleepUntil(Inventory::isEmpty, 5000, 600);
+                Logger.log("(dyes) (bankRedberries) deposited redberries");
+            }
         }
-        return 1000;
+        return 600;
     }
 
 
